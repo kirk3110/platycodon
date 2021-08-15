bad5ac677e270382aba7ea7ad8bf0034e02675bd
kirk3110 <saito.kiriko@gmail.com>
2021-08-15 22:23:56 +0900

    [Update]pathでroomごとの切り替えを可能に

--- src/main/java/com/kirk3110/platycodon/controller/ChatController.java
+++ src/main/java/com/kirk3110/platycodon/controller/ChatController.java
@@ -3,11 +3,13 @@ package com.kirk3110.platycodon.controller;
 import com.kirk3110.platycodon.model.Message;
 import com.kirk3110.platycodon.service.MessageServiceImpl;
 import java.util.List;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ChatController {
@@ -18,18 +20,19 @@ public class ChatController {
         this.messageService = messageService;
     }
 
-    @GetMapping("/chat")
-    public String get(Model model) {
-        model.addAttribute("savedMessages", messageService.fetchMessages(1));
+    @GetMapping("/chat/{roomId}")
+    public String get(@PathVariable Integer roomId, Model model) {
+        model.addAttribute("savedMessages", messageService.fetchMessages(roomId));
         return "chat";
     }
 
-    @MessageMapping("/message")
-    @SendTo("/receive/messages")
-    public List<Message> receive(Message message) throws Exception {
+    @MessageMapping("/message/{roomId}")
+    @SendTo("/receive/messages/{roomId}")
+    public List<Message> receive(@DestinationVariable Integer roomId, Message message)
+        throws Exception {
         Thread.sleep(1000);
         List<Message> messages = this.messageService.analyzeMessage(message);
-        this.messageService.saveMessages(messages, 1);
+        this.messageService.saveMessages(messages, roomId);
         return messages;
     }
 }
