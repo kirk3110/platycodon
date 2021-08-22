d4439a1984473b09af36174e9209497c8b820870
kirk3110 <saito.kiriko@gmail.com>
2021-08-22 19:48:27 +0900

    [Fix]バリデーションいらなさそうなので一旦削除（必要になったら復活させる）

--- src/main/java/com/kirk3110/platycodon/model/Message.java
+++ src/main/java/com/kirk3110/platycodon/model/Message.java
@@ -1,23 +1,18 @@
 package com.kirk3110.platycodon.model;
 
 import java.util.Date;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.web.util.HtmlUtils;
 
 @Data
 public class Message {
 
-    @Size(max = 100)
     private String name;
 
     private String statement;
 
-    @NotEmpty
     private Date sentAt;
 
-    @Size(max = 7)
     private String color;
 
     public Message(String name, String statement, Date sentAt, String color) {
